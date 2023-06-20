ce72f6d46f073256bd5ab54dc22c7b4c2397df8a
riyap3197 <riyap3197@gmail.com>
2023-06-19 22:32:39 -0400

    display lists

---
+++ src/main/java/springframework/springWebApp/controllers/AuthorController.java
@@ -0,0 +1,22 @@
+package springframework.springWebApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.springWebApp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository){
+        this.authorRepository=authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+}
---
+++ src/main/java/springframework/springWebApp/controllers/BookController.java
@@ -0,0 +1,23 @@
+package springframework.springWebApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.springWebApp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
+    }
+
+}
